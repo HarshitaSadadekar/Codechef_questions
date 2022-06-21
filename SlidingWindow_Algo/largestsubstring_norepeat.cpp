b334e56e195dbd4380642e89ea37bfb4ac2326e0
Kikii <harshitasadadekar@gmail.com>
2022-06-21 19:08:01 +0530

    largest substring with no repeat

---
+++ SlidingWindow_Algo/largestsubstring_norepeat.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestSubstringNoRepeat(string s){
+    unordered_map<char,int> um;
+    int i=0,j=0,max_len=INT_MIN;
+
+    while(j < s.length()){
+        um[s[j]]++;
+        if(um.size() > j-i+1){
+            j++;
+        }
+        else if(um.size() == j-i+1){
+            max_len = max(max_len, j-i+1);
+            j++;
+        }
+        else if(um.size() < j-i+1){
+            while(um.size() < j-i+1){
+                um[s[i]]--;
+                if(um[s[i]] == 0){
+                    um.erase(s[i]);
+                }
+                i++;
+            }
+        }
+    }
+    return max_len;
+}
+
+int main(){
+    string str;
+    cin>>str;
+    cout<<largestSubstringNoRepeat(str);
+    return 0;
+}
