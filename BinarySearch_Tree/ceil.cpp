53020384cf23fab634fadab54e818447c550457e
Kikii <harshitasadadekar@gmail.com>
2022-07-30 18:37:31 +0530

    Binary Search Trees, ceil of BST

---
+++ BinarySearch_Tree/ceil.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int findCeil(BinaryTreeNode<int> *root, int key){
+        int ceil = -1;
+        while(root){
+            if(root->data == key){
+                ceil = root->data;
+                return ceil;
+            }
+            
+            if(key > root->data){
+                root = root->right;
+            }
+            else{
+                ceil = root->data;
+                root = root->left;
+            }
+        }
+        return ceil;
+    }
+}
